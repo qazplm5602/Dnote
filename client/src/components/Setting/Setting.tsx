a51b6f600cea7dcdf24380536fe12a7ce05dfd9a
qazplm5602 <t01048187045@gmail.com>
2024-09-30 00:47:52 +0900

    설정 개인정보 및 보안 화면

--- client/src/components/Setting/Setting.tsx
+++ client/src/components/Setting/Setting.tsx
@@ -2,6 +2,7 @@ import { Link, Route, Routes } from 'react-router-dom';
 import SettingGeneric from './Generic';
 import style from './setting.module.css';
 import SettingPassword from './SettingPassword';
+import SettingSecurity from './SettingSecurity';
 
 export default function Setting() {
     return <main className={`screen_container ${style.main}`}>
@@ -11,6 +12,7 @@ export default function Setting() {
             <Routes>
                 <Route path='/' element={<SettingGeneric />} />
                 <Route path='/password' element={<SettingPassword />} />
+                <Route path='/security' element={<SettingSecurity />} />
                 <Route path='/test' element={<><article><h2>테스트 인데요?</h2></article></>} />
             </Routes>
         </section>
@@ -20,7 +22,7 @@ export default function Setting() {
 function Menu() {
     return <article className={style.menu}>
         <Link to="/setting/"><div className={style.active}>일반</div></Link>
-        <Link to="/setting/test"><div>개인정보 및 보안</div></Link>
+        <Link to="/setting/security"><div>개인정보 및 보안</div></Link>
         <Link to="/setting/password"><div>비밀번호 변경</div></Link>
     </article>
 }
---
+++ client/src/components/Setting/SettingSecurity.tsx
@@ -0,0 +1,11 @@
+import style from './settingSecurity.module.css';
+
+export default function SettingSecurity() {
+    return <article>
+        <h3 className={style.title}>개인정보 및 보안</h3>
+    </article>;
+}
+
+function SliderToggle() {
+    
+}
