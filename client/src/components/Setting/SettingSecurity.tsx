53d7693679348bbfb61f74a506da9f734a48b83f
domi <domi@domi.kr>
2024-10-03 06:11:49 +0900

    토글 스위치 만듬

--- client/src/components/Setting/SettingSecurity.tsx
+++ client/src/components/Setting/SettingSecurity.tsx
@@ -1,11 +1,21 @@
 import style from './settingSecurity.module.css';
 
 export default function SettingSecurity() {
-    return <article>
-        <h3 className={style.title}>개인정보 및 보안</h3>
+    return <article className={style.main}>
+        <SliderToggle id='test222' title='팔로우 공개' desc='다른 사람들도 나의 프로필에서 팔로우 수를 볼 수 있습니다.' />
     </article>;
 }
 
-function SliderToggle() {
-    
+function SliderToggle({ id, title, desc, className }: { id: string, title: string, desc: string, className?: string }) {
+    return <div className={`${style.slider_toggle} ${className || ""}`}>
+        <section>
+            <div className={style.head}>{title}</div>    
+            <div className={style.sub}>{desc}</div>    
+        </section>
+
+        <section className={style.toggle}>
+            <input type="checkbox" id={id} />
+            <label htmlFor={id}></label>
+        </section>
+    </div>
 }
