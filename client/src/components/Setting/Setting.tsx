09f3375cb33c93741efe3624c0d4fe5c47cc9a5a
domi <domi@domi.kr>
2024-10-03 06:27:37 +0900

    세팅 메뉴

--- client/src/components/Setting/Setting.tsx
+++ client/src/components/Setting/Setting.tsx
@@ -1,4 +1,4 @@
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes, useLocation } from 'react-router-dom';
 import SettingGeneric from './Generic';
 import style from './setting.module.css';
 import SettingPassword from './SettingPassword';
@@ -19,10 +19,21 @@ export default function Setting() {
     </main>;
 }
 
+const menuList = [
+    ['', '일반'],
+    ['security', '개인정보 및 보안'],  
+    ['password', '비밀번호 변경']  
+];
+
+function checkUrl(url: string, id: string) {
+    const regex = new RegExp(`^\\/setting\\/${id}\\/?$`);
+    return regex.test(url);
+}
+
 function Menu() {
+    const { pathname } = useLocation();
+
     return <article className={style.menu}>
-        <Link to="/setting/"><div className={style.active}>일반</div></Link>
-        <Link to="/setting/security"><div>개인정보 및 보안</div></Link>
-        <Link to="/setting/password"><div>비밀번호 변경</div></Link>
+        {menuList.map(v => <Link key={v[0]} to={`/setting/${v[0]}`}><div className={checkUrl(pathname, v[0]) ? style.active : ''}>{v[1]}</div></Link>)}
     </article>
 }
