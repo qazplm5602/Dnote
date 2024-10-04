19b2e04437094113077db1740352f8e352616be9
domi <domi@domi.kr>
2024-10-04 22:48:40 +0900

    아이콘 버튼 재활용

--- client/src/components/Recycle/Button.tsx
+++ client/src/components/Recycle/Button.tsx
@@ -4,3 +4,12 @@ export default function Button({ children, className = [] }: { children?: React.
     className.push(style.default_btn);
     return <button className={className.join(' ')}>{children}</button>;
 }
+
+export function IconButton({ icon, children, className = [] }: { icon: string, children?: React.ReactNode, className?: string[] }) {
+    className.push(style.icon_btn);
+    
+    return <button className={className.join(' ')}>
+        <img className={style.ico} src={icon} />
+        {children}
+    </button>
+}
