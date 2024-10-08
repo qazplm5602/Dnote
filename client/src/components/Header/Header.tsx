9c9837cc19ab023ecb5be62343c06d80fe49771a
domi <domi@domi.kr>
2024-10-09 02:30:17 +0900

    계정 메뉴

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -3,6 +3,10 @@ import style from './header.module.css';
 
 import Logo from '../../assets/Dnote.svg'
 import SearchIcon from '../../assets/icons/ic-outline-search.svg';
+import GearIcon from '../../assets/icons/gear.svg';
+import LogoutIcon from '../../assets/icons/logout.svg';
+import NameTag from '../NameTag/NameTag';
+import IconText from '../Recycle/IconText';
 // import NameTag from '../NameTag/NameTag';
 
 export default function Header() {
@@ -16,11 +20,13 @@ export default function Header() {
 
             <div className={style.line}></div>
 
-            {/* <NameTag /> */}
+            <NameTag />
 
-            <Button className={[style.link_btn].join(' ')} text='로그인' link='/login' />
-            <Button className={[style.link_btn, style.register].join(' ')} text='회원가입' link='/signup' />
+            {/* <Button className={[style.link_btn].join(' ')} text='로그인' link='/login' />
+            <Button className={[style.link_btn, style.register].join(' ')} text='회원가입' link='/signup' /> */}
         </section>
+
+        <AccountMenu />
     </header>;
 }
 
@@ -28,4 +34,15 @@ function Button({ text, link, className }: { text: string, link: string, classNa
     return <Link to={link}>
         <button className={className}>{text}</button>
     </Link>
+}
+
+function AccountMenu() {
+    return <section className={style.account_menu}>
+        <AccountMenuButton icon={GearIcon} text='설정' />
+        <AccountMenuButton icon={LogoutIcon} text='로그아웃' />
+    </section>;
+}
+
+function AccountMenuButton({ href, icon, text }: { href?: string, icon: string, text: string }) {
+    return <button><IconText icon={icon} text={text} /></button>
 }
