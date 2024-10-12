d4f7c81137d250dfb9cab4357e9a1ec525f04eab
domi <domi@domi.kr>
2024-10-12 14:56:42 +0900

    스프링 추가

---
+++ server/src/main/java/com/domi/dnote/DnoteApplication.java
@@ -0,0 +1,13 @@
+package com.domi.dnote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DnoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DnoteApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/domi/dnote/DnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domi.dnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
