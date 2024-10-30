9077949eeffb4a127d24b5e56eb7b3a4e5785b3a
qazplm5602 <t01048187045@gmail.com>
2024-10-30 18:19:52 +0900

    #6 임시 저장 불러오기

--- server/src/main/java/com/domi/dnote/Controller/PostTempController.java
+++ server/src/main/java/com/domi/dnote/Controller/PostTempController.java
@@ -1,8 +1,10 @@
 package com.domi.dnote.Controller;
 
+import com.domi.dnote.DTO.BasePostDTO;
 import com.domi.dnote.DTO.PostUploadDTO;
 import com.domi.dnote.Entity.PostTemp;
 import com.domi.dnote.Entity.User;
+import com.domi.dnote.Exception.UserException;
 import com.domi.dnote.Service.PostTempService;
 import com.domi.dnote.Service.UserService;
 import com.domi.dnote.Util.MiscUtil;
@@ -11,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/post/temp")
+@RequestMapping("/api/post/temp")
 public class PostTempController {
     final UserService userService;
     final PostTempService postTempService;
@@ -33,5 +35,24 @@ public class PostTempController {
         return tempId;
     }
 
-//    @GetMapping("/{id}")
+    @GetMapping("/{id}")
+    BasePostDTO getTempPost(@PathVariable("id") String id) {
+        User user = userService.getCurrentUser();
+        PostTemp post =  postTempService.getById(id);
+
+
+        // 자기꺼 아님
+        if (post.getUser() != user) {
+            throw new UserException(UserException.Type.NEED_PERMISSION);
+        }
+
+        BasePostDTO data = new BasePostDTO();
+        data.setTitle(post.getTitle());
+        data.setTags(post.getTags());
+        data.setThumbnail(post.getThumbnail());
+        data.setContent(post.getContent());
+        data.setCreated(post.getCreated());
+
+        return data;
+    }
 }
