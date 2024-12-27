e9fa30711b412b176e83f6ca6446a2caf802cc43
domi <domi@domi.kr>
2024-12-27 19:58:12 +0900

    #15 컨텍스트 메뉴 스토어 연동

--- client/src/components/ContextMenu/Box.tsx
+++ client/src/components/ContextMenu/Box.tsx
@@ -1,15 +1,27 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import style from './contextMenu.module.scss';
 import { forwardRef } from "react";
 import { RootState } from '../Redux/Store';
+import { ContextMenuState, setMenuHide } from '../Redux/ContextMenuStateSlice';
 
 const ContextMenuBox = forwardRef<HTMLDivElement>((_, ref) => {
-    // const data = useSelector<RootState>(v => v.contextMenu.pos);
+    const data = useSelector<RootState, ContextMenuState>(v => v.contextMenu);
+    const dispatch = useDispatch();
+    const posStyle = {
+        left: `${data.pos.left}px`,
+        top: `${data.pos.top}px`
+    };
+    const onItemClick = function(idx: number) {
+        const callback = data.menus[idx].callback;
+        dispatch(setMenuHide()); // 메뉴 닫고
+        callback();
+    }
+    const onBoxClick = function(e: React.MouseEvent<HTMLElement>) {
+        e.stopPropagation();
+    }
 
-    return <section ref={ref} className={style.context_menu}>
-        <div>테스트 1</div>
-        <div>테스트 2</div>
-        <div>테스트 3</div>
+    return <section ref={ref} className={style.context_menu} style={posStyle} onClick={onBoxClick}>
+        {data.menus.map((v, i) => <div key={v.text} onClick={() => onItemClick(i)}>{v.text}</div>)}
     </section>
 });
 
--- client/src/components/ContextMenu/ContextMenu.tsx
+++ client/src/components/ContextMenu/ContextMenu.tsx
@@ -1,10 +1,11 @@
 import style from './contextMenu.module.scss';
 
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../Redux/Store";
 import { CSSTransition } from 'react-transition-group';
 import ContextMenuBox from './Box';
 import { useRef } from 'react';
+import { setMenuHide } from '../Redux/ContextMenuStateSlice';
 
 const animStyles = {
     enter: style.enter,
@@ -16,9 +17,14 @@ const animStyles = {
 export default function ContextMenu() {
     const show = useSelector<RootState, boolean>(v => v.contextMenu.show);
     const nodeRef = useRef<HTMLDivElement>(null);
+    const dispatch = useDispatch();
+
+    const onBgClick = function() {
+        dispatch(setMenuHide()); // 메뉴 닫음음
+    }
 
     return <CSSTransition nodeRef={nodeRef} timeout={300} unmountOnExit in={show} classNames={animStyles}>
-        <article className={style.main}>
+        <article className={style.main} onClick={onBgClick}>
             <ContextMenuBox ref={nodeRef} />
         </article>
     </CSSTransition>;
