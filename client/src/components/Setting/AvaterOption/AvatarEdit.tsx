bf2a6d6382e853a5af985edc726a97e0f2d55fc5
domi <domi@domi.kr>
2025-01-03 10:12:55 +0900

    #20 아바타 편집 프론트엔드

--- client/src/components/Setting/AvaterOption/AvatarEdit.tsx
+++ client/src/components/Setting/AvaterOption/AvatarEdit.tsx
@@ -4,35 +4,57 @@ import Button, { SpinnerButton } from '../../Recycle/Button';
 
 import zoomIco from '../../../assets/icons/zoom.svg';
 import crawlIco from '../../../assets/icons/crawl.svg';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
+import { useNotify } from '../../Notify/NotifyContext';
 
 type Props = {
-    image: File
+    image: File,
+    onClose?: () => void
 }
-export default function SettingAvatarEdit({ image }: Props) {
+export default function SettingAvatarEdit(props: Props) {
     return <section className={style.main}>
-        <Dialog image={image} />
+        <Dialog {...props} />
     </section>
 }
 
-function Dialog({ image }: Props) {
+function Dialog({ image, onClose }: Props) {
     const [ scale, setScale ] = useState(100);
+    const [ loading, setLoading ] = useState(false);
+    const editor = useRef<AvatarEditor>(null);
+        const notify = useNotify();
     
     const onScaleChange = function(e: React.ChangeEvent<HTMLInputElement>) {
         setScale(Number(e.target.value));
+    }
+    const imageUploadToServer = function(blob: Blob | null) {
+        if (blob === null) {
+            setLoading(false);
+            notify('Error', "이미지 변환 중 오류가 발생하였습니다.", 5000);
+            return;
+        }
+
+        // ... 백엔드 구현 뒤
+    }
+    const onSave = function() {
+        const canvas = editor.current?.getImageScaledToCanvas();
+        if (!canvas) return;
+
+        setLoading(true);
+        canvas.toBlob(blob => imageUploadToServer(blob));
     }
 
     return <div className={style.box}>
         <h1>아바타 편집</h1>
 
         <AvatarEditor
-            image={"https://domi.kr/img/logo.png"}
+            image={image}
             className={style.view}
             width={200}
             height={200}
             borderRadius={99}
             border={5}
             scale={scale / 100}
+            ref={editor}
         />
 
         <section className={style.zoom}>
@@ -42,8 +64,8 @@ function Dialog({ image }: Props) {
         </section>
 
         <section className={style.interaction}>
-            <SpinnerButton loading={true}>저장</SpinnerButton>
-            <Button className={[style.close]}>닫기</Button>
+            <SpinnerButton loading={loading} onClick={onSave}>저장</SpinnerButton>
+            <Button className={[style.close]} onClick={onClose} disabled={loading}>닫기</Button>
         </section>
     </div>
 }
--- client/src/components/Setting/AvaterOption/AvaterOption.tsx
+++ client/src/components/Setting/AvaterOption/AvaterOption.tsx
@@ -3,19 +3,47 @@ import Button from '../../Recycle/Button';
 import style from '../settingGeneric.module.css';
 import AvatarEditor from 'react-avatar-editor'
 import SettingAvatarEdit from './AvatarEdit';
+import { useRef, useState } from 'react';
+import { useNotify } from '../../Notify/NotifyContext';
 
 type Props = {
     avater: string | null
 };
 export default function SettingProfileOption({ avater }: Props) {
+    const [ uploadImage, setUploadImage ] = useState<File | null>(null);
+    const fileRef = useRef<HTMLInputElement>(null);
+    const notify = useNotify();
+
+    const onUploadClick = function() {
+        fileRef.current?.click();
+    }
+    const onFileChange = function(e: React.ChangeEvent<HTMLInputElement>) {
+        if (e.target.files === null) return;
+
+        const file = e.target.files[0];
+        const isImage = file.type.startsWith("image/");
+        
+        if (!isImage) {
+            notify('Error', "이미지만 가능합니다.", 5000);
+            return;
+        }
+
+        setUploadImage(file);
+    }
+    const onAvatarEditClose = function() {
+        setUploadImage(null);
+    }
+    
+
     return <section className={style.profile}>
     <h3>프로필 이미지</h3>
 
     <section className={style.group}>
         <img className={style.avater} src={getProfileURL(avater)} />
-        <Button className={[style.upload_btn]}>업로드<span>권장 사이즈 512x512</span></Button>
+        <input type="file" ref={fileRef} onChange={onFileChange} className={style.file} />
+        <Button className={[style.upload_btn]} onClick={onUploadClick}>업로드<span>권장 사이즈 512x512</span></Button>
     </section>
 
-    <SettingAvatarEdit image={new File([], '')} />
+    {uploadImage && <SettingAvatarEdit image={uploadImage} onClose={onAvatarEditClose} />}
 </section>;
 }
