d3548f247f10c810698b28240867b3213f6ab61c
domi <domi@domi.kr>
2025-01-08 20:21:39 +0900

    #22 post 삭제 기능

--- client/src/components/Post/OtherMenu/OtherMenu.tsx
+++ client/src/components/Post/OtherMenu/OtherMenu.tsx
@@ -5,9 +5,11 @@ import style from '../post.module.css';
 import { useDispatch, useSelector } from "react-redux";
 import { setMenuShow } from "../../Redux/ContextMenuStateSlice";
 import { useRef } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { RootState } from "../../Redux/Store";
 import { LoginState } from "../../Redux/LoginStateSlice";
+import request from "../../Utils/request";
+import { useNotify } from "../../Notify/NotifyContext";
 
 export default function PostOtherMenu() {
     const { id, user } = useParams();
@@ -15,6 +17,9 @@ export default function PostOtherMenu() {
 
     const dispatch = useDispatch();
     const domiRef = useRef<HTMLButtonElement>(null);
+
+    const navigate = useNavigate();
+    const notify = useNotify();
     
     if (id === undefined || user === undefined || !loginUser.logined || loginUser.id !== Number(user)) return;
 
@@ -22,7 +27,15 @@ export default function PostOtherMenu() {
         
     }
     const onRemove = function() {
-        
+        const accept = confirm("게시물을 삭제하시겠습니까?");
+        if (!accept) return;
+
+        request(`post/remove/${id}`, { method: "DELETE" })
+        .then(() => {
+            notify("Success", "작업이 완료되었습니다.", 5000);
+            navigate(`/user/${user}`);
+        })
+        .catch(() => notify("Error", "네트워크 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.", 5000));
     }
     
     const onClick = function() {
