d5c869c60cc59b87a0e9c6fa792991f4b42675bb
domi <domi@domi.kr>
2025-01-16 16:55:22 +0900

    오류 처리

--- AI_play/main.js
+++ AI_play/main.js
@@ -1,5 +1,6 @@
 const { maxAgent, newDelay } = require('./config.json');
 const { getRandomAgent } = require("./util/agentList");
+const chalk = require("chalk");
 
 const processAgents = new Set();
 
@@ -7,7 +8,11 @@ function agentCreate() {
     const agent = getRandomAgent();
     processAgents.add(agent);
 
-    agent.start().finally(() => {
+    agent.start()
+    .catch((e) => {
+        console.log(`[main] ${agent.agentName} ${chalk.red("처리 실패")}`, e);
+    })
+    .finally(() => {
         processAgents.delete(agent);
         setTimeout(agentCreate, newDelay);
     });
