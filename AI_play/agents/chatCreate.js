94fef0a3c5f291bd23ec589439bb07299050c898
domi <domi@domi.kr>
2025-01-17 17:04:07 +0900

    AI 잘못된 대답 감지

--- AI_play/agents/chatCreate.js
+++ AI_play/agents/chatCreate.js
@@ -25,9 +25,7 @@ class ChatCreateAI extends AIbase {
         const age = getRandomNumber(1, 80);
 
         this.log(`${user.email}(${user.name}) 가 /post/${post.owner_id}/${post.id}(${owner_name}) 글의 댓글을 작성중...`);
-        let result = await requestAI(`${post.content_preview}에 대한 글을 평가를 하거나 피드백을 하거나 질문을 짧게 해줘.`, {
-            system: `당신의 이름은 ${user.name} 입니다. 당신은 글을 읽고 있는 독자 입니다. 글의 주인은 ${owner_name} 입니다. Dnote 사이트의 블로그 글을 보고 있습니다. 댓글 처럼 표현하세요. 당신의 나이는 ${age}살 입니다. 나이에 맞는 표현을 사용하세요. 한국어(Korean)으로 대답해야합니다.`
-        });
+        const result = await this.createContent(post.content_preview, user.name, age, owner_name);
 
         const accessToken = createAccessToken(user.email);
         const { status, data } = await request(`post/${post.owner_id}/${post.id}/chat`, { method: "POST", body: result }, accessToken);
@@ -37,6 +35,22 @@ class ChatCreateAI extends AIbase {
         }
 
         this.log(chalk.green("댓글 작성 성공"), `/post/${post.owner_id}/${post.id}(${owner_name})의 글`, result);
+    }
+
+    async createContent(content_preview, name, age, owner_name, count) {
+        const response = await requestAI(`${content_preview}에 대한 글을 평가를 하거나 피드백을 하거나 질문을 짧게 해줘.`, {
+            system: `당신의 이름은 ${name} 입니다. 당신은 글을 읽고 있는 독자 입니다. 글의 주인은 ${owner_name} 입니다. Dnote 사이트의 블로그 글을 보고 있습니다. 댓글 처럼 표현하세요. 당신의 나이는 ${age}살 입니다. 나이에 맞는 표현을 사용하세요. 한국어(Korean)으로 대답해야합니다.`
+        });
+        
+        if (response.indexOf(content_preview) !== -1) { // 똑같은 내용 들어있음 (잘못된거 분명함 ㄹㅇㅇ)
+            if (count > 5) {
+                throw new Error("댓글 생성 실패.");
+            }
+
+            return this.createContent(content_preview, name, age, owner_name, (count || 0) + 1);
+        }
+
+        return response;
     }
 }
 
--- AI_play/agents/chatReplyCreate.js
+++ AI_play/agents/chatReplyCreate.js
@@ -21,9 +21,7 @@ class ChatReplyCreateAI extends AIbase {
         const age = getRandomNumber(1, 80);
 
         this.log(`${chalk.yellow(`${user.email}(${user.name}) -> chat ${chat.id}(${owner_name}) 답장중...`)}`);
-        const response = await requestAI(`${content}에 대한 댓글을 평가를 하거나 피드백을 하거나 질문을 하거나 공감을 70글자 이내로 답글(Reply) 해줄래?`, {
-            system: `당신의 이름은 ${user.name} 입니다. 댓글(Comment)의 주인은 ${owner_name} 입니다. Dnote 사이트의 블로그 댓글을 보고 있습니다. 당신의 나이는 ${age}살 입니다. 나이에 맞는 표현을 사용하세요. 한국어(Korean)으로 대답해야합니다.`
-        });
+        const response = await this.createContent(content, user.name, age, owner_name);
 
         const accessToken = createAccessToken(user.email);
 
@@ -34,6 +32,22 @@ class ChatReplyCreateAI extends AIbase {
         }
 
         this.log(chalk.green("답장 성공!"), `${user.email}(${user.name}) -> chat ${chat.id}(${owner_name})`, response);
+    }
+
+    async createContent(chat, name, age, owner_name, count) {
+        const response = await requestAI(`${chat}에 대한 댓글을 평가를 하거나 피드백을 하거나 질문을 하거나 공감을 70글자 이내로 답글(Reply) 해줄래?`, {
+            system: `당신의 이름은 ${name} 입니다. 댓글(Comment)의 주인은 ${owner_name} 입니다. Dnote 사이트의 블로그 댓글을 보고 있습니다. 당신의 나이는 ${age}살 입니다. 나이에 맞는 표현을 사용하세요. 한국어(Korean)으로 대답해야합니다.`
+        });
+        
+        if (response.indexOf(chat) !== -1) { // 똑같은 내용 들어있음 (잘못된거 분명함 ㄹㅇㅇ)
+            if (count > 5) {
+                throw new Error("답글 생성 실패.");
+            }
+
+            return this.createContent(chat, name, age, owner_name, (count || 0) + 1);
+        }
+
+        return response;
     }
 }
 
