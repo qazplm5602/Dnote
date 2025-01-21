c3606275d218e4d835e0a2dac40c920781287fd8
domi <domi@domi.kr>
2025-01-21 16:46:11 +0900

    #36 main sitemap 불러오깅

--- sitemap_builder/modules/mainSitemap.ts
+++ sitemap_builder/modules/mainSitemap.ts
@@ -1,10 +1,20 @@
+import { XMLParser } from "fast-xml-parser";
+import fs from 'fs';
+import path from 'path';
+import config from '../_config.ts';
+import { urlEntity } from "./userSitemap.ts";
+
 type reserveData = {
     userId: number,
     action: 'add' | 'remove' | 'update'
 }
+type IndexedSitemap = {
+    [key: string]: urlEntity
+}
 
 let reserveUsers: reserveData[] = [];
 
+const xmlParser = new XMLParser();
 
 export function registerUser(id: number, action: reserveData['action']) {
     reserveUsers.push({ userId: id, action: action });
@@ -14,8 +24,40 @@ export function clear() {
     reserveUsers = [];
 }
 
-export function updateSitemap() {
+export async function updateSitemap() {
     const currentUserIds = new Set<number>();
+    const currentSitemap = IndexingSitemap(await getSitemap() || []);
 
-    console.log("reserveUsers", reserveUsers);
+    console.log("reserveUsers", reserveUsers, currentSitemap);
+}
+
+async function getSitemap(): Promise<urlEntity[] | undefined> {
+    return new Promise((resolve, reject) => {
+        const onSuccess = function(err: NodeJS.ErrnoException | null, data: string) {
+            if (err) {
+                if (err.code === "ENOENT") {
+                    resolve(undefined);
+                    return;
+                }
+
+                reject(err);
+                return;
+            }
+
+            const sitemap = xmlParser.parse(data).sitemapindex.sitemap;
+            if (sitemap)
+                resolve(sitemap instanceof Array ? sitemap : [ sitemap ]);
+            else
+                resolve(undefined);
+        }
+
+        fs.readFile(path.join(path.resolve(), config.sitemapPath, 'main.xml'), { encoding: "utf-8" }, onSuccess);
+    });
+}
+
+function IndexingSitemap(list: urlEntity[]): IndexedSitemap {
+    const result: IndexedSitemap = {};
+    list.forEach(v => result[v.loc] = v);
+
+    return result;
 }
