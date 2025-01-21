d104fe27aca65afa09b28887615b28ce7319ab1b
domi <domi@domi.kr>
2025-01-22 02:50:43 +0900

    #32 헤더 모바일 메뉴 버튼 구현함

--- client/src/components/Header/Menu.tsx
+++ client/src/components/Header/Menu.tsx
@@ -1,7 +1,33 @@
 import style from './header.module.css';
 import { IconButton } from "../Recycle/Button";
 import menuIcon from '../../assets/icons/menu.svg';
+import { useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { setMenuShow } from '../Redux/ContextMenuStateSlice';
+import { useNavigate } from 'react-router-dom';
 
 export default function HeaderMenuButton() {
-    return <IconButton icon={menuIcon} className={[style.more]} />;
+    const btnRef = useRef<HTMLButtonElement>(null);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const onMenuShow = function() {
+        if (btnRef.current === null) return;
+
+        const pos = {
+            top: btnRef.current.offsetTop + 15,
+            left: btnRef.current.offsetLeft - 150
+        };
+        const menus = [
+            { text: "로그인", callback: () => navigate("/login") },
+            { text: "회원가입", callback: () => location.href = "https://domi.kr/bbs/register.php" }
+        ];
+
+        dispatch(setMenuShow({
+            menus,
+            pos
+        }));
+    }
+
+    return <IconButton icon={menuIcon} className={[style.more]} domiRef={btnRef} onClick={onMenuShow} />;
 }
--- client/src/components/Recycle/ScreenWidth/ScreenWidth.tsx
+++ client/src/components/Recycle/ScreenWidth/ScreenWidth.tsx
@@ -3,12 +3,14 @@ import { useEffect, useState } from "react";
 export function useScreenWidth(maxWidth: number) {
     const [ small, setSmall ] = useState(false);
 
-    const onResize = function(e: UIEvent) {
+    const onResize = function() {
         setSmall(window.innerWidth < maxWidth);
     }
     
     useEffect(() => {
         window.addEventListener("resize", onResize);
+        onResize();
+
         return () => window.removeEventListener("resize", onResize);
     }, []);
 
