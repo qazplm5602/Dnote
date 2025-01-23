58eccd5b2cf5c19630c5f459cf1b5809fa1c52fc
domi <domi@domi.kr>
2025-01-24 03:30:44 +0900

    빌드 청크 나눔

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -19,5 +19,32 @@ export default defineConfig({
         changeOrigin: true
       },
     }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id: string) {
+          // if (id.includes("axios")) {
+          //   return "@networking-vendor";
+          // }
+          if (id.includes("prosemirror") || id.includes("prismjs")) {
+            return "@code-plugin";
+          }
+          if (id.includes("toast-ui")) {
+            let prefix = '';
+
+            if (id.includes("editor-plugin-"))
+              prefix = "-plugin";
+            else if (id.includes("editor-viewer"))
+              prefix = "-viewer";
+
+            return `@toast-ui${prefix}`;
+          }
+          // if (id.includes("node_modules/react/") || id.includes("node_modules/react-dom/") || id.includes("node_modules/react-redux")) {
+          //   return "@react-vendor";
+          // }
+        }
+      }
+    }
   }
 })
