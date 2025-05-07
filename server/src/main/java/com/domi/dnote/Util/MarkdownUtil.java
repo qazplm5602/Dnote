f563caddad18a16e3cd020652f455513aa312b80
domi <domi@domi.kr>
2025-05-07 19:49:40 +0900

    #40 백엔드 커스텀 렌더링 콘텐츠 제외

--- server/src/main/java/com/domi/dnote/Util/MarkdownUtil.java
+++ server/src/main/java/com/domi/dnote/Util/MarkdownUtil.java
@@ -7,12 +7,18 @@ import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
 import org.commonmark.renderer.text.TextContentRenderer;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MarkdownUtil {
     public final static List<Extension> extensions = List.of(TablesExtension.create());
+//    private final static String CUSTOM_PREFIX = "$$";
 
     public static Node parse(String markdown) {
         Parser parser = Parser.builder()
@@ -33,10 +39,45 @@ public class MarkdownUtil {
         return renderer.render(document);
     }
 
+    // p 태그 안에 커스텀 말고 다른것도 있었응
+    public static Document removeCustomRenderer(Document root) {
+        for (Element element : root.select("p")) {
+            String content = element.text();
+
+            Pattern pattern = Pattern.compile("\\$\\$[\\s\\S]*?\\$\\$");
+            Matcher matcher = pattern.matcher(content);
+
+            while (matcher.find()) {
+                content = content.substring(0, matcher.start()) + content.substring(matcher.end());
+                matcher = pattern.matcher(content);
+            }
+
+            if (content.isEmpty())
+                element.remove(); // 아무것도 없지롱
+            else
+                element.text(content);
+
+//            if (content.startsWith((CUSTOM_PREFIX))) {
+//
+//                // 아니 전부 다 커스텀 ㄷㄷ
+//                if (content.endsWith(CUSTOM_PREFIX)) {
+//                    element.remove();
+//                    continue;
+//                }
+//
+//                // 첫 $$ <-- 이거 제외한거
+//                String nonStartContent = content.substring(CUSTOM_PREFIX.length());
+//
+//            }
+        }
+
+        return root;
+    }
+
     public static String extractText(String markdown) {
         markdown = replaceSafeContent(markdown);
         String html = convertHtml(markdown);
-        return Jsoup.parse(html).text(); // html에서 text만
+        return removeCustomRenderer(Jsoup.parse(html)).text(); // html에서 text만
     }
 
     public static String replaceSafeContent(String markdown) {
